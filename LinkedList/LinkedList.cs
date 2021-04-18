6ac16a95a026a43daf1cd9afc4b2b03d861ffdc7
unknown <lakshmehta06@gmail.com>
2021-04-18 22:53:10 +0530

    [Laksh add to linked list]

--- LinkedList/LinkedList.cs
+++ LinkedList/LinkedList.cs
@@ -8,6 +8,23 @@ namespace LinkedList
     {
 
         internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+
 
 
         static void Main(string[] args)
